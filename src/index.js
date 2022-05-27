5471290269139fc8b02c80d11d281ab8fc6fc5f0
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-05-26 20:31:30 -0400

    added form, open and close functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read}`
-  }
+// DISPLAY FORM //
+const form = document.querySelector(".bookForm");
+const overlay = document.querySelector("#overlay");
+
+function displayForm() {
+  form.classList.add("active");
+  overlay.classList.add("active");
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'read');
-console.log(hobbit.info())
-const donQuixote = new Book('Don Quixote', 'Miguel De Cervantes', 928, 'have not read');
-console.log(donQuixote.info())
-const taleOfTwoCities = new Book('Tale of Two Cities', 'Charles Dickens', 304, 'read');
-console.log(taleOfTwoCities.info())
+const addButton = document.getElementById("addBook");
+addButton.addEventListener('click', displayForm);
+
+// CLOSE FORM
+function closeForm() {
+  form.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const closeButton = document.querySelector("[data-close-button]");
+closeButton.addEventListener("click", closeForm);
+
+// SUBMIT FORM
+const submitButton = document.querySelector("");
